9282d2b9b83e3228c077d932020425d89adad338
devsocha <konrad.socha5@gmail.com>
2022-12-28 20:35:33 +0100

    Dodanie route do nav

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+<<<<<<< HEAD
+=======
+Route::get('options',[\App\Http\Controllers\technican\TechnicanController::class,'options'
+])->name('options');
+>>>>>>> a836ad5 ( Dodanie route do nawigacji aplikacji)
 
 /* Technican */
 Route::get('home/technican',[\App\Http\Controllers\technican\TechnicanController::class,'index'
@@ -19,6 +24,10 @@ Route::get('requests/technican',[\App\Http\Controllers\technican\TechnicanContro
 
 /* Head Technican */
 Route::get('settings/admin',[\App\Http\Controllers\technican\TechnicanController::class,'index'
+<<<<<<< HEAD
 ])->name('admin/settings');
+=======
+])->name('technican.settings');
+>>>>>>> a836ad5 ( Dodanie route do nawigacji aplikacji)
 
 /* Head Admin */
