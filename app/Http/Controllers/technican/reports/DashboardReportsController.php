fe55b737ad9029e89302deb9e9bc4d9091baec07
Konrad Socha <konrad.socha5@gmail.com>
2023-01-20 15:52:53 +0100

    Dodanie Controllera do obslugi raportow

---
+++ app/Http/Controllers/technican/reports/DashboardReportsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\technican\reports;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardReportsController extends Controller
+{
+    //
+}
