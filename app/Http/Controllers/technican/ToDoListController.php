9d1e9f35021103f2d142ecd1b3f0cc4dd625bcf8
Konrad Socha <konrad.socha5@gmail.com>
2023-03-10 17:15:21 +0100

    utworzenie migracji, modelu i controllera dla todolist

---
+++ app/Http/Controllers/technican/ToDoListController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\technican;
+
+use App\Http\Controllers\Controller;
+
+class ToDoListController extends Controller
+{
+    public function index(){
+
+    }
+    public function addToDo(){
+
+    }
+
+}
