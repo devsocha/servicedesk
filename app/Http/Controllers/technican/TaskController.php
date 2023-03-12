48ebd007ae5fa671f786585a1df1085f7d8a87cf
Konrad Socha <konrad.socha5@gmail.com>
2023-03-12 22:23:19 +0100

    Dodanie controllera do obslugi zadan dla zgloszen

---
+++ app/Http/Controllers/technican/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\technican;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function addTaskView($id){
+        return;
+    }
+}
