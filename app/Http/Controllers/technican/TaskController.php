44828e9657fc28ee2e01ca6030e77c75585b0605
Konrad Socha <konrad.socha5@gmail.com>
2023-03-15 22:47:03 +0100

    Dodanie funkcji umieszczania taska w bazie danych

--- app/Http/Controllers/technican/TaskController.php
+++ app/Http/Controllers/technican/TaskController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\technican;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function addTaskView($id){
         return;
+    }
+    public static function addTask(Request $request){
+        try{
+            $request->validate([
+                'idRequest'=>'required',
+                'title'=>'required',
+                'desc'=>'desc'
+            ]);
+            Task::create([
+                'request_id'=>$request->idRequest,
+                'title'=>$request->title,
+                'description'=>$request->desc,
+            ]);
+            return redirect()->back()->with(['success'=>'Poprawnie dodano zadanie']);
+        }catch (\Exception $e){
+            return redirect()->back()->with(['error'=>'Wystąpił błąd, spróbuj ponownie później']);
+        }
+
     }
     //TODO modul zadan dla technika do requestu
 }
