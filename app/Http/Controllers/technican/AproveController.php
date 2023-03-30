36c45dc9014d1755b95019a17e2cabc6ba5f5e28
Konrad Socha <konrad.socha5@gmail.com>
2023-03-30 22:40:17 +0200

    utworzenie funkcji wysylajacej maila z potwierdzeniem zgloszenia

--- app/Http/Controllers/technican/AproveController.php
+++ app/Http/Controllers/technican/AproveController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\technican;
 
 use App\Http\Controllers\Controller;
+use App\Mail\WebsiteEmail;
+use App\Models\Aprove;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class AproveController extends Controller
 {
-    //
+    public function addNewAprover(){
+
+    }
+    public function sendMail($idUser,$idAprove){
+        $token = hash('sha256',time());
+        try{
+            Aprove::where('id',$idAprove)->update([
+                'token'=> $token,
+            ]);
+            $user = User::where('id',$idUser)->first();
+            $subject = 'Your Accept or Reject request';
+            $link = url('/aprove/request/'.$idAprove.'/'.$token);
+            $body = 'Hi '.$user->imie.' '.$user->nazwisko.',<br> You have to accept or reject a request. </br> You can do it from this link: <a href="'.$link.'">Link</a> ';
+            return Mail::to($user->email)->send(new WebsiteEmail($subject,$body));
+        }catch (\Exception $e){
+
+        }
+    }
 }
